4ae2d744b257332a00898d65af93adf33b296699
Elisabeth Hansen <bethhansen92@gmail.com>
2020-02-11 20:19:57 -0800

    all challenges passing.

--- code-challenges/code-challenge-04-regular-expressions/challenges-04.test.js
+++ code-challenges/code-challenge-04-regular-expressions/challenges-04.test.js
@@ -26,7 +26,7 @@ Return an array containing all the matches.
 ------------------------------------------------------------------------------------------------ */
 
 const isCapitalized = (str) => {
-  // Solution code here...
+  return str.match(/[A-Z]\w+/gm) || [];
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -36,7 +36,12 @@ Write a function named citiesAtoJ that takes in an array of city names and uses 
 ------------------------------------------------------------------------------------------------ */
 
 const citiesAtoJ = (arr) => {
-  // Solution code here...
+  const newArr = [];
+  const regex = (/^[A-J]\w+/gm);
+  arr.forEach( str => {
+    regex.test(str) ? newArr.push(str.match(regex)[0]) : null;
+  });
+  return newArr;
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -52,7 +57,8 @@ Do not use the vertical bar (pipe) in your pattern.
 ------------------------------------------------------------------------------------------------ */
 
 const matchMonth = (input) => {
-  // Solution code here...
+  const regex = (/^[oO]ct(ober)?$/gm);
+  return regex.test(input);
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -66,7 +72,8 @@ The expected output of "Hello, and have a wonderful day!" is ["and ", "have ", "
 ------------------------------------------------------------------------------------------------ */
 
 const noPunctuation = str => {
-  // Solution code here...
+  const regex = (/\w+\s+/gm);
+  return str.match(regex);
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -82,7 +89,8 @@ For example, 'Welcome to Code 301!' will return 'W_lc_m_ t_ C_d_ 301!'.
 ------------------------------------------------------------------------------------------------ */
 
 let hangman = (str) => {
-  // Solution code here...
+  const regex = (/[aeiou]/gmi);
+  return str.replace(regex, '_');
 };
 
 /* ------------------------------------------------------------------------------------------------
